1173da748ca348e508fd85eaf9cfd084e01590c3
Oohira Suguru <suguru.13579515@gmail.com>
2021-02-15 08:29:53 +0900

    STudy python

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 
 game_greeting = "ようこそ！\nこのGameは天才プログラマーが開発した冒険ゲームです。\nぜひ楽しんでください！！１"
 question_your_name = "あなたのお名前を教えてください"
+question_your_name2 = "空白は入力を受け付けません。もう一度名前を入力してください"
 caretaker_talk_1 = "さんって言うのですね。\n素敵なお名前です。早速ですが,Monstorが出てきたので戦ってください！"
 caretaker_talk_2 = "敵を倒しました\nこれからも敵がたくさん出てくるので頑張ってください"
 battle_talk_1 = "攻撃する場合は大文字の1を入力してください"
@@ -15,8 +16,16 @@ battle_talk_2 = "敵に攻撃する！！！！！"
 battle_talk_3 = "敵を倒しました！！！！！"
 battle_talk_3 = "入力し直してください"
 
+def input_cheack_blank(brave_name):
+    if(brave_name == " "):
+        print(question_your_name2)
+    elif(brave_name == "　")
+        print(question_your_name2)
+
+
 print(game_greeting)
 brave_name = input(question_your_name)
+input_cheack_blank(brave_name)
 print(brave_name + caretaker_talk_1)
 
 while True:
